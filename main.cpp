7e7614b759d0497733a88f87f064e3916eb55d8e
Pablo <pablo@pabloCorp.es>
2014-11-17 13:04:05 +0100

    Cambio chachi 2 de la mañana

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <QCoreApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     //HOLAAAAAA
+    qDebug() << "Cambio 1 de la mañana.";
 
     return a.exec();
 }
